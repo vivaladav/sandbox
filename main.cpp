7ab6c07eaae40537e3039e685a7cae53400d680d
Davide Coppola <vivaladav@gmail.com>
2016-10-16 19:54:55 +0100

    Added 7 simple lines to main.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 int main(int argc, char * argv[])
 {
+	int a = 1;
+	int b = 2;
+
+	std::cout << "a: " << a << std::endl;
+	std::cout << "b: " << rba << std::endl;
+	std::cout << "a + b: " << (a + b) << std::endl;
+	std::cout << "a - b: " << (a - b) << std::endl;
 
 	return 0;
 }
