3e481ce957519dd0dad2b65883baf8504b18a8a4
Davide Coppola <vivaladav@gmail.com>
2016-10-18 01:05:06 +0100

    Removing comments 3.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ int main(int argc, char * argv[])
 	int a = 1;
 	int b = 2;
 
-	// second add in test branch
 	std::cout << "a: " << a << std::endl;
 	std::cout << "b: " << rba << std::endl;
 	std::cout << "a + b: " << (a + b) << std::endl;
